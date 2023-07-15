66a9e1961bb351076496213a892d507980175b8a
Kajeloki <piotrkw200@wp.pl>
2023-07-15 22:07:52 +0200

    szybki start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Header />
+      <Meals />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import ChartButton from "./UI/ChartButton";
+
+const Header =()=>{
+return <div>
+    <h2>To jest nagłówek</h2>
+    <ChartButton />
+    </div>
+}
+export default Header;
