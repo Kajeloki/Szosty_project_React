7e2c53d7d0d5724a09372fe4b05fdf3a314f4026
Kajeloki <piotrkw200@wp.pl>
2023-07-16 11:02:57 +0200

    pierwsze prace przy Header

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import ChartButton from "./UI/ChartButton";
-
-const Header =()=>{
-return <div>
-    <h2>To jest nagłówek</h2>
-    <ChartButton />
-    </div>
-}
-export default Header;
--- src/components/UI/ChartButton.js
+++ src/components/UI/ChartButton.js
@@ -1,4 +1,11 @@
+import CartIcon from './CartIcon';
+import classes from './ChartButton.module.css';
+
 const ChartButton =(props)=>{
-    return <button></button>
+    return <button className={classes.button}>
+        <span className={classes.icon}><CartIcon /></span>
+        <span>{props.children}</span>
+        <span className={classes.badge}> 3</span>
+        </button>
 }
 export default ChartButton;
