4445b5bc115bcf973ae109f2b89bde90777666ff
Kajeloki <piotrkw200@wp.pl>
2023-08-07 20:11:56 +0200

    pobieranie listy dań z bazy danych

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,37 @@
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import Meal from '../Meal/Meal';
+import { useEffect, useState } from 'react';
 
 const AvailableMeals =(props)=>{
-    const DUMMY_MEALS = [
-        {
-          id: '1',
-          name: 'Sushi',
-          description: 'Finest fish and veggies',
-          price: 22.99,
-        },
-        {
-          id: '2',
-          name: 'Schnitzel',
-          description: 'A german specialty!',
-          price: 16.5,
-        },
-        {
-          id: '3',
-          name: 'Barbecue Burger',
-          description: 'American, raw, meaty',
-          price: 12.99,
-        },
-        {
-          id: '4',
-          name: 'Green Bowl',
-          description: 'Healthy...and green...',
-          price: 18.99,
-        },
-      ];
+  const [meals, setMeals]=useState([]);
 
-      const mealsList=DUMMY_MEALS.map(meal=>
+  useEffect(()=>{
+    const fetchHandler = async ()=>{
+      const response = await fetch('https://strona-do-zamawiania-jedzenia-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+      const data = await response.json();
+      
+      for(const key in data)
+      loadedMeals.push({
+    id: key,
+    name: data[key].name,
+    description: data[key].description,
+    price: data[key].price
+  
+    });
+    setMeals(loadedMeals)
+    };
+
+    fetchHandler();
+    },[])
+  const loadedMeals = [];
+ 
+
+      const mealsList=meals.map(meal=>
         <Meal name={meal.name} description={meal.description} price={meal.price} key={meal.id} id={meal.id}/>
+        
       );
+      
 return <Card className={classes.meals}>
     <ul>
         {mealsList}
